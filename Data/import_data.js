61e346af0c44fe86a4521e4fc5a4316fe9a68df4
IamTomide <paulinaebunoluwa@gmail.com>
2024-08-23 13:52:11 +0100

    env changes

--- Data/import_data.js
+++ Data/import_data.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: "./.env"});
+dotenv.config({path: "../.env"});
 
 const fs = require('fs');
 const Van = require('../model/vanmodel');
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({path: "./.env"});
+dotenv.config();
 
 const app = require('./app');
 const mongoose = require('mongoose');
